7ff317a3ba23881129ce34cfb8f1c167f8e81c4d
stooged <stooged@users.noreply.github.com>
2022-11-01 18:31:06 +0800

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# dWin
+
+Arduino library to communicate with the <a href=https://www.dwin-global.com/>Dwin T5L LCD Screens</a>
+
+this is still a WIP but has the basic functionality to read and write vp addresses.
+
+
+The user interface is created with <a href=https://www.dwin-global.com/tool/>DGUS</a> 
+
+
---
+++ examples/test/test.ino
@@ -0,0 +1,73 @@
+#include <SoftwareSerial.h>
+#include <dWin.h>
+
+SoftwareSerial LCD_Serial(3, 2);
+
+dWin dwin;
+
+int ii = 134;
+long ll = 20234;
+
+void read_Serial() {
+  uint8_t Length = LCD_Serial.available();
+  char Data[Length];
+  for (int i = 0; i <= Length; i++) {
+    Data[i] = LCD_Serial.read();
+  }
+
+  if ((Data[0] << 8) | Data[1] == FrameHeader) {
+    switch ((Data[4] << 8) | Data[5]) {
+      case 0x5600:
+        break;
+      case 0x5500:
+        Serial.println("5500 key");
+        ll++;
+        dwin.setText(0x0A00, String(ll).c_str());
+
+        Serial.println(dwin.read_vp(0x0B00));
+
+        //  dwin.setPage(2);
+
+        //dwin.setBrightness(80,30,1);
+
+        dwin.set4bLong(0x0B00, ll);
+        break;
+      case 0x0002:
+        // Serial.println("0002 key");
+        ii++;
+        dwin.setText(0x0A00, String(ii).c_str());
+        dwin.set2bInt(0x0C00, ii);
+        break;
+      case 0x1003:
+        dwin.setText(0x0A00, "hello world");
+        break;
+    }
+  }
+}
+
+
+void update_values() {
+  //update values on loop
+}
+
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  LCD_Serial.begin(115200);
+
+  dwin.set_Serial(LCD_Serial);
+}
+
+
+
+void loop() {
+  if (LCD_Serial.available() > 0) {
+    read_Serial();
+  }
+  delay(100);
+  update_values();
+  delay(50);
+}
